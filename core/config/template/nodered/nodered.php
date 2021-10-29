d4d16e5471dde5815d8f346a26c2588b06ff82a2
Loïc <loic@jeedom.com>
2021-10-29 17:53:20 +0200

    escape special char

--- core/config/template/nodered/nodered.php
+++ core/config/template/nodered/nodered.php
@@ -2,10 +2,8 @@
 function nodered_post($_eqLogic, $_values) {
     $file = '/var/lib/docker/volumes/tmp_node-red-data/_data/settings.js';
     shell_exec('sudo sed -i \'/adminAuth: {type: "credentials"/d\' ' . $file);
-    $password = trim(shell_exec('sudo docker exec nodered  node -e "console.log(require(\'bcryptjs\').hashSync(process.argv[1], 8));" ' . $_values['PASSWORD']));
-
+    $password = trim(shell_exec('sudo docker exec nodered  node -e "console.log(require(\'bcryptjs\').hashSync(process.argv[1], 8));" ' . escapeshellarg($_values['PASSWORD'])));
     $cmd = 'sudo sed -i \'/\/\/adminAuth: {/i adminAuth: {type: "credentials",users: [{username: "admin",password: "' . $password . '",permissions: "*"}]},\' ' . $file;
-    echo $cmd;
     shell_exec($cmd);
     $_eqLogic->restart();
 }
