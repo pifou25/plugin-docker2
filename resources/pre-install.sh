921d505fd707bf088c26b2f2bf77e502620e4300
Loïc <loic@jeedom.com>
2021-11-02 13:49:53 +0100

    add more log

--- resources/pre-install.sh
+++ resources/pre-install.sh
@@ -3,13 +3,14 @@
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --no-tty --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 arch=`dpkg --print-architecture`;
+echo "Docker arch found : "$arch
 if [[ $arch == "armhf" ]]; then 
 	echo "deb [arch=armhf signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-fi
-if [[ $arch = "x86_64" ]]; then
+elif [[ $arch = "x86_64" ]]; then
 	echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-fi
-if [[ $arch = "arm64" ]]; then
-	 echo "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+elif [[ $arch = "arm64" ]]; then
+	echo "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+else
+	echo "Your architecture is not compatible with Docker"
 fi
 apt-get update
