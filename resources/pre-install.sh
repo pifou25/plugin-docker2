92e8bb7070e6ecd060390b503e02653994abb3f9
Loïc <loic@jeedom.com>
2021-11-02 13:34:39 +0100

    bugfix

--- resources/pre-install.sh
+++ resources/pre-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --no-tty --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+wget -qO - https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 arch=`dpkg --print-architecture`;
 if [[ $arch == "armhf" ]]; then 
 	echo "deb [arch=armhf signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
