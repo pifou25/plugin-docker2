b42ee6fa767e5e44dafc45a5d4675f654e34a571
Salvialf <jeuxalf@live.fr>
2022-11-15 13:04:24 +0100

    review eqLogics display

--- desktop/php/docker2.php
+++ desktop/php/docker2.php
@@ -2,7 +2,6 @@
 if (!isConnect('admin')) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
-// Déclaration des variables obligatoires
 $plugin = plugin::byId('docker2');
 sendVarToJS('eqType', $plugin->getId());
 $eqLogics = eqLogic::byType($plugin->getId());
@@ -10,10 +9,8 @@ sendVarToJS('docker2_internal_ip', network::getNetworkAccess('internal', 'ip'));
 ?>
 
 <div class="row row-overflow">
-	<!-- Page d'accueil du plugin -->
 	<div class="col-xs-12 eqLogicThumbnailDisplay">
 		<legend><i class="fas fa-cog"></i> {{Gestion}}</legend>
-		<!-- Boutons de gestion du plugin -->
 		<div class="eqLogicThumbnailContainer">
 			<div class="cursor eqLogicAction logoPrimary" data-action="add">
 				<i class="fas fa-plus-circle"></i>
@@ -36,40 +33,40 @@ sendVarToJS('docker2_internal_ip', network::getNetworkAccess('internal', 'ip'));
 				<span class="txtColor">{{Code}}</span>
 			</div>
 		</div>
-		<legend><i class="fas fa-table"></i> {{Mes Dockers}}</legend>
+		<legend><i class="fab fa-docker"></i> {{Mes Dockers}}</legend>
 		<?php
 		if (count($eqLogics) == 0) {
-			echo '<br/><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun équipement docker n\'est paramétré, cliquer sur "Ajouter" pour commencer}}</div>';
+			echo '<br><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun Docker trouvé, cliquer sur "Ajouter" pour commencer}}</div>';
 		} else {
-			// Champ de recherche
 			echo '<div class="input-group" style="margin:5px;">';
-			echo '<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic"/>';
+			echo '<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic">';
 			echo '<div class="input-group-btn">';
 			echo '<a id="bt_resetSearch" class="btn" style="width:30px"><i class="fas fa-times"></i></a>';
 			echo '<a class="btn roundedRight hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>';
 			echo '</div>';
 			echo '</div>';
-			// Liste des équipements du plugin
 			echo '<div class="eqLogicThumbnailContainer">';
 			foreach ($eqLogics as $eqLogic) {
 				$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
 				echo '<div class="eqLogicDisplayCard cursor ' . $opacity . '" data-eqLogic_id="' . $eqLogic->getId() . '">';
-				echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+				echo '<img src="' . $plugin->getPathImgIcon() . '">';
 				echo '<br>';
 				echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+				echo '<span class="hiddenAsCard displayTableRight hidden">';
+				echo ($eqLogic->getConfiguration('create::mode', '') != '') ? '<span class="label label-info">{{Docker}} ' . $eqLogic->getConfiguration('docker_number') . ' - ' . $eqLogic->getConfiguration('create::mode') . '</span>' : '';
+				echo ($eqLogic->getConfiguration('saveMount') == 1) ? '<i class="fas fa-save icon_green" title="{{Docker sauvegardé}}"></i> ' : '';
+				echo ($eqLogic->getIsVisible() == 1) ? '<i class="fas fa-eye" title="{{Equipement visible}}"></i>' : '<i class="fas fa-eye-slash" title="{{Equipement non visible}}"></i>';
+				echo '</span>';
 				echo '</div>';
 			}
 			echo '</div>';
 		}
 		?>
-	</div> <!-- /.eqLogicThumbnailDisplay -->
+	</div>
 
-	<!-- Page de présentation de l'équipement -->
 	<div class="col-xs-12 eqLogic" style="display: none;">
-		<!-- barre de gestion de l'équipement -->
 		<div class="input-group pull-right" style="display:inline-flex;">
 			<span class="input-group-btn">
-				<!-- Les balises <a></a> sont volontairement fermées à la ligne suivante pour éviter les espaces entre les boutons. Ne pas modifier -->
 				<a class="btn btn-sm btn-default" id="bt_docker2Assistant"><i class="fas fa-people-carry"></i><span class="hidden-xs"> {{Assistant}}</span>
 				</a><a class="btn btn-sm btn-default eqLogicAction" data-action="configure"><i class="fas fa-cogs"></i><span class="hidden-xs"> {{Configuration avancée}}</span>
 				</a><a class="btn btn-sm btn-default eqLogicAction" data-action="copy"><i class="fas fa-copy"></i><span class="hidden-xs"> {{Dupliquer}}</span>
@@ -78,17 +75,14 @@ sendVarToJS('docker2_internal_ip', network::getNetworkAccess('internal', 'ip'));
 				</a>
 			</span>
 		</div>
-		<!-- Onglets -->
+
 		<ul class="nav nav-tabs" role="tablist">
 			<li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fas fa-arrow-circle-left"></i></a></li>
 			<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
 			<li role="presentation"><a href="#commandtab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-list"></i> {{Commandes}}</a></li>
 		</ul>
 		<div class="tab-content">
-			<!-- Onglet de configuration de l'équipement -->
 			<div role="tabpanel" class="tab-pane active" id="eqlogictab">
-				<!-- Partie gauche de l'onglet "Equipements" -->
-				<!-- Paramètres généraux de l'équipement -->
 				<form class="form-horizontal">
 					<fieldset>
 						<div class="col-lg-6">
@@ -96,8 +90,8 @@ sendVarToJS('docker2_internal_ip', network::getNetworkAccess('internal', 'ip'));
 							<div class="form-group">
 								<label class="col-sm-3 control-label">{{Nom de l'équipement}}</label>
 								<div class="col-sm-7">
-									<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-									<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement}}" />
+									<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;">
+									<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement}}">
 								</div>
 							</div>
 							<div class="form-group">
@@ -121,7 +115,7 @@ sendVarToJS('docker2_internal_ip', network::getNetworkAccess('internal', 'ip'));
 									<?php
 									foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
 										echo '<label class="checkbox-inline">';
-										echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+										echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '">' . $value['name'];
 										echo '</label>';
 									}
 									?>
@@ -130,12 +124,14 @@ sendVarToJS('docker2_internal_ip', network::getNetworkAccess('internal', 'ip'));
 							<div class="form-group">
 								<label class="col-sm-3 control-label">{{Options}}</label>
 								<div class="col-sm-7">
-									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked />{{Activer}}</label>
-									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked />{{Visible}}</label>
+									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked>{{Activer}}</label>
+									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked>{{Visible}}</label>
 								</div>
 							</div>
+
+							<legend><i class="fas fa-cogs"></i> {{Paramètres spécifiques}}</legend>
 							<div class="form-group">
-								<label class="col-sm-3 control-label">{{Docker host}}</label>
+								<label class="col-sm-3 control-label">{{Docker hôte}}</label>
 								<div class="col-sm-7">
 									<select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="docker_number">
 										<?php
@@ -150,13 +146,13 @@ sendVarToJS('docker2_internal_ip', network::getNetworkAccess('internal', 'ip'));
 							<div class="form-group">
 								<label class="col-sm-3 control-label">{{Nom du conteneur}}</label>
 								<div class="col-sm-7">
-									<input class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="name" />
+									<input class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="name">
 								</div>
 							</div>
 							<div class="form-group">
-								<label class="col-sm-3 control-label">{{Sauvegarder les mount}}</label>
+								<label class="col-sm-3 control-label">{{Sauvegarder le conteneur}}</label>
 								<div class="col-sm-7">
-									<input type="checkbox" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="saveMount" />
+									<input type="checkbox" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="saveMount">
 								</div>
 							</div>
 							<div class="form-group">
@@ -164,87 +160,87 @@ sendVarToJS('docker2_internal_ip', network::getNetworkAccess('internal', 'ip'));
 								<div class="col-sm-7">
 									<select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="create::mode">
 										<option value="manual">{{Manuel}}</option>
-										<option value="jeedom_run">{{Jeedom docker run}}</option>
-										<option value="jeedom_compose">{{Jeedom docker compose}}</option>
+										<option value="jeedom_run">{{Jeedom Docker run}}</option>
+										<option value="jeedom_compose">{{Jeedom Docker Compose}}</option>
 									</select>
 								</div>
 							</div>
 							<div class="form-group create_mode jeedom_run">
 								<label class="col-sm-3 control-label">{{Commande de création}}</label>
 								<div class="col-sm-7">
-									<textarea rows="12" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="create::run"></textarea>
+									<textarea class="eqLogicAttr form-control autogrow" data-l1key="configuration" data-l2key="create::run"></textarea>
 								</div>
 							</div>
 							<div class="form-group create_mode jeedom_compose">
-								<label class="col-sm-3 control-label">{{Docker compose}}</label>
-								<div class="col-sm-9">
-									<textarea rows="20" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="create::compose"></textarea>
+								<label class="col-sm-3 control-label">{{Docker Compose}}</label>
+								<div class="col-sm-7">
+									<textarea class="eqLogicAttr form-control autogrow" data-l1key="configuration" data-l2key="create::compose"></textarea>
 								</div>
 							</div>
 						</div>
-						<div class="col-lg-6">
 
-							<legend><i class="fas fa-exclamation"></i> {{Action}}</legend>
-							<center>
+						<div class="col-lg-6">
+							<br>
+							<div class="text-center">
 								<span class="input-group-btn">
-									<a class="btn btn-sm btn-success roundedLeft" id="bt_dockerBackup"><i class="fas fa-save"></i><span class="hidden-xs"> {{Sauvegarder le docker}}</span>
-									</a><a class="btn btn-sm btn-warning" id="bt_dockerRestore"><i class="fas fa-database"></i><span class="hidden-xs"> {{Restaurer le docker}}</span>
+									<a class="btn btn-sm btn-success roundedLeft" id="bt_dockerBackup"><i class="fas fa-save"></i><span class="hidden-xs"> {{Sauvegarder le conteneur}}</span>
+									</a><a class="btn btn-sm btn-warning" id="bt_dockerRestore"><i class="fas fa-database"></i><span class="hidden-xs"> {{Restaurer le conteneur}}</span>
 									</a><a class="btn btn-sm btn-default" id="bt_dockerDownloadBackup"><i class="fas fa-download"></i><span class="hidden-xs"> {{Télécharger sauvegarde}}</span>
-									</a><span class="btn btn-default btn-file" style="height: 28px;line-height: 14px">
+									</a><span class="btn btn-sm btn-default btn-file">
 										<i class="fas fa-upload"></i> {{Envoyer sauvegarde}}<input id="bt_dockerUploadBackup" type="file" name="file" data-url="plugins/docker2/core/ajax/docker2.ajax.php?action=backupupload">
-									</span><a class="btn btn-sm btn-default" id="bt_dockerLog"><i class="far fa-file"></i><span class="hidden-xs"> {{Logs docker}}</span>
+									</span><a class="btn btn-sm btn-default" id="bt_dockerLog"><i class="far fa-file"></i><span class="hidden-xs"> {{Logs conteneur}}</span>
 									</a>
 								</span>
-							</center>
+							</div>
 							<legend><i class="fas fa-info"></i> {{Informations}}</legend>
 							<div class="form-group">
 								<label class="col-sm-3 control-label">{{ID}}</label>
-								<div class="col-sm-9">
-									<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="id" style="font-size : 1em"></span>
+								<div class="col-sm-7">
+									<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="id"></span>
 								</div>
 							</div>
 							<div class="form-group">
 								<label class="col-sm-3 control-label">{{Commande}}</label>
-								<div class="col-sm-9">
-									<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="command" style="font-size : 1em"></span>
+								<div class="col-sm-7">
+									<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="command"></span>
 								</div>
 							</div>
 							<div class="form-group">
 								<label class="col-sm-3 control-label">{{Créé}}</label>
 								<div class="col-sm-3">
-									<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="createdAt" style="font-size : 1em"></span>
+									<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="createdAt"></span>
 								</div>
 								<label class="col-sm-3 control-label">{{Taille}}</label>
 								<div class="col-sm-3">
-									<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="size" style="font-size : 1em"></span>
+									<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="size"></span>
 								</div>
 							</div>
 							<div class="form-group">
 								<label class="col-sm-3 control-label">{{Ports}}</label>
 								<div class="col-sm-3">
-									<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="ports" style="font-size : 1em"></span>
+									<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="ports"></span>
 								</div>
 								<label class="col-sm-3 control-label">{{Networks}}</label>
 								<div class="col-sm-3">
-									<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="networks" style="font-size : 1em"></span>
+									<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="networks"></span>
 								</div>
 							</div>
 							<div class="form-group">
 								<label class="col-sm-3 control-label">{{Montages}}</label>
-								<div class="col-sm-9">
-									<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="mounts" style="font-size : 1em"></span>
+								<div class="col-sm-7">
+									<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="mounts"></span>
 								</div>
 							</div>
 							<div class="form-group">
 								<label class="col-sm-3 control-label">{{Image}}</label>
-								<div class="col-sm-9">
-									<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="image" style="font-size : 1em"></span>
+								<div class="col-sm-7">
+									<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="image"></span>
 								</div>
 							</div>
 							<div class="form-group">
 								<label class="col-sm-3 control-label">{{Accès}}</label>
-								<div class="col-sm-9">
-									<a target="_blank" id="link_dockerUrl" style="font-size : 1em">Test</a>
+								<div class="col-sm-7">
+									<a target="_blank" id="link_dockerUrl">Test</a>
 								</div>
 							</div>
 						</div>
@@ -257,23 +253,23 @@ sendVarToJS('docker2_internal_ip', network::getNetworkAccess('internal', 'ip'));
 					<table id="table_cmd" class="table table-bordered table-condensed">
 						<thead>
 							<tr>
-								<th>{{Id}}</th>
-								<th>{{Nom}}</th>
+								<th class="hidden-xs" style="min-width:50px;width:70px;"> ID</th>
+								<th style="min-width:150px;width:300px;">{{Nom}}</th>
 								<th>{{Type}}</th>
-								<th>{{Paramètres}}</th>
-								<th>{{Options}}</th>
-								<th>{{Action}}</th>
+								<th>{{Etat}}</th>
+								<th style="min-width:260px;width:400px;">{{Options}}</th>
+								<th style="min-width:80px;width:180px;">{{Actions}}</th>
 							</tr>
 						</thead>
 						<tbody>
 						</tbody>
 					</table>
 				</div>
-			</div><!-- /.tabpanel #commandtab-->
+			</div>
 
-		</div><!-- /.tab-content -->
-	</div><!-- /.eqLogic -->
+		</div>
+	</div>
 </div>
 
-<?php include_file('desktop', 'docker2', 'js', 'docker2'); ?>
-<?php include_file('core', 'plugin.template', 'js'); ?>
+<?php include_file('desktop', 'docker2', 'js', 'docker2');
+include_file('core', 'plugin.template', 'js'); ?>
